169d4ee32bcf6051827445f9c5f7aa318d55bbad
yeji <skql4128@naver.com>
2023-05-16 23:39:33 +0900

    0515 필기

---
+++ React/0515/src/App.js
@@ -0,0 +1,26 @@
+import RouteStudy from './RouterSolve/RouteStudy';
+import RouteSolve from './RouterSolve/RouteSolve';
+import Counter from './HookUseEffect/TryUseEffect';
+import Time from './HookUseEffect/Quiz2';
+import Counter2 from './HookUesRef/TryUseRef';
+import TryUseRefDom from './HookUesRef/TryUseRefDom';
+import TryUseMemo from './HookUseMemo/TryUseMemo';
+
+function App() {
+  return (
+    function App() {
+      return (
+        <div className="App">
+          {/* <RouteStudy/> */}
+          {/* <RouteSolve/> */}
+          {/* <Counter/> */}
+          {/* <Time/> */}
+          {/* <Counter2/> */}
+          {/* <TryUseRefDom/> */}
+          <TryUseMemo/>
+        </div>
+      );
+    }
+  );
+}
+export default App;
